b4e3b8c8a0dd51dd9f20d17a8e75d05814ea7d84
timchipperfield <timchipperfield@gmail.com>
2016-07-19 13:20:56 +0100

    unfinished github-omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+resources :profiles
+  root to: 'profiles#index'
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  # get 'welcome#index'
+  #
+  # root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
