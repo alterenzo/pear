90ed6a35eeb61fa5c987d573c9e27d20b9005749
joestephens <joe.stephens@live.co.uk>
2016-07-19 17:45:45 +0100

    Added profile new page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
          :omniauth_providers => [:github]
 
+  has_and_belongs_to_many :tags
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,4 @@
 describe User, type: :model do
   it { should validate_presence_of(:about_me) }
-  it { should have_many(:tags) }
+  it { should have_and_belong_to_many(:tags) }
 end
