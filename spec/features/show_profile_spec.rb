856e60c9ca4c9149d4be2feb922ac50c4be0e069
joestephens <joe.stephens@live.co.uk>
2016-07-20 13:35:39 +0100

    Added navbar links

--- spec/features/show_profile_spec.rb
+++ spec/features/show_profile_spec.rb
@@ -1,13 +1,23 @@
-require 'rails_helper'
+require "rails_helper"
 
-feature 'profiles' do
+feature "profiles" do
 
-  context 'viewing other peoples profiles' do
-    scenario 'should display users profile details' do
+  context "viewing other peoples profiles" do
+    scenario "should display users profile details" do
       user = FactoryGirl.create(:user)
       login_as(user)
-      visit "/profiles/#{user.id}"
+      visit profile_path(user)
       expect(page).to have_content("My name is James Brown")
+    end
+  end
+
+  context "viewing my own profile" do
+    scenario "clicking on email should display my profile" do
+      user = FactoryGirl.create(:user)
+      login_as(user)
+      visit root_path
+      click_link user.email
+      expect(current_path).to eq profile_path(user)
     end
   end
 
