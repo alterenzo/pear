9d50bd0f12e2e389736a67cb1a0b89dfacd71366
joestephens <joe.stephens@live.co.uk>
2016-08-09 20:56:40 +0100

    configured env variables and callbacks for Heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -269,7 +269,7 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :github, "151f5ab778fe5079dccd", ENV['GITHUB_APP_SECRET'],
-                  callback_url: "http://localhost:3000/users/auth/github/callback",
+  config.omniauth :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_APP_SECRET'],
+                  callback_url: "http://findapear.herokuapp.com/users/auth/github/callback",
                   scope: "user,repo"
 end
